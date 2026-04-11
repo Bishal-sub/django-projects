ff7e5e20c1df954c5770c51ce50e1c501c360f3f
bishal <173034130+Bishal-sub@users.noreply.github.com>
2026-04-11 18:58:36 +0545

    e-commerce added

---
+++ e-commerce/e_commerce/store/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    productname = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.productname
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='variants')
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+
+    stock = models.PositiveIntegerField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.product.productname} - {self.size.name} - {self.color.name}'
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    img = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.product.productname
