ce51997d0fa3fbeb4242f04c831dd464b98421e6
bishal <173034130+Bishal-sub@users.noreply.github.com>
2026-05-15 08:26:37 +0545

    expense tracker

--- Expense_Tracker/expense_tracker/accounts/views.py
+++ Expense_Tracker/expense_tracker/accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        if User.objects.filter(username = username).exists():
+            messages.error(request,"UserALready exist")
+        
+    
+    
+    
+    
+    
+    
+    return render(request,'register.html')
